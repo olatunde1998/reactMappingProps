93b7a7cead1db22ded0e7a874f079524e143860a
Olatunde <olatunde336@gmail.com>
2022-08-05 19:01:17 +0100

    first commit

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,28 +1,43 @@
-import React from "react";
-import notes from "../notes";
-import Card from "./card";
+import React, { useState} from "react";
+
+
 
 
 
 function App(){
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  function updateFName(event){
+    setFName(event.target.value);
+  }
+  function updateLName(event){
+    setLName(event.target.value);
+  }
+
   return (
-  <div>
-    <h1 className="headings"> Keeper</h1>
-    <div className="boxContainer">
-      {notes.map(note =>
-   
-        <Card 
-          key = {note.key}
-          title = {note.title}
-          content = {note.content}
-          imgURL = {note.imgURL}
-          learnMore = {note.button}
-          
-        />
-  
-      )}
-      
+  <div >
+    <h1 className="headings">Geodev-</h1>
+    <div className="boxCard">
+      <h2>Hello {fName} {lName} </h2>
+        
+      <input 
+          onChange={updateFName} 
+          type = "text" 
+          value={fName} 
+          placeholder ="Surname?"/>
+
+      <input 
+          onChange={updateLName} 
+          type = "text" 
+          value={lName} 
+          placeholder ="Last Name?"/>
+
+      <button> Submit</button>
     </div>
+   
+      
+   
   </div>
   );
 }
